6a5692e5a7a09bca0328f61e058b2a34e54302c2
Thatielen Oliveira <thatielenoliveira@hotmail.com>
2020-12-27 16:27:46 -0400

    chore: add first selenium test and add required packages

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-console.log('Test Successfully Executed!!');
-process.exit(0);
+const webdriver = require('selenium-webdriver');
+
+(async function test() {
+    const driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions({
+        "browserName": "chrome",
+        "chromeOptions": {
+            "args": ["--no-sandbox"]
+        }
+    }).build();
+
+    try {
+        await driver.get('https://prosas.com.br/editais');
+    }
+    catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+    finally {
+        await driver.quit();
+        console.log("Test Successfully Executed!!");
+        process.exit(0);
+    }
+})();
