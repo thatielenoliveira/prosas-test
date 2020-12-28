f8c8e1520bc823910008f7343b41286b8066d566
Thatielen Oliveira <thatielenoliveira@hotmail.com>
2020-12-28 01:45:29 -0400

    chore: add more tests and refactor test script

--- main.js
+++ main.js
@@ -1,23 +1,111 @@
-const webdriver = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 
 (async function test() {
-    const driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions({
-        "browserName": "chrome",
-        "goog:chromeOptions": {
-            "args": ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
-        }
-    }).build();
+    const driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions({
+            "browserName": "chrome",
+            "goog:chromeOptions": {
+                "args": ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
+            }
+        }).build();
 
+    let failedTests = 0;
+    let totalTests = 0;
+    
+    /**
+     * Test open editais page
+     * Should open the editais page
+     */
     try {
         await driver.get('https://prosas.com.br/editais');
     }
     catch (error) {
+        console.log('Failed at Accessing Editais');
         console.log(error);
-        process.exit(1);
+        
+        failedTests += 1;
     }
     finally {
-        await driver.quit();
-        console.log("Test Successfully Executed!!");
+        totalTests += 1;
+    }
+
+    /**
+     * Test search using only the name / description filter
+     * Itens should contain the search key in its name or sponsor's name
+     */
+    try {
+        /**
+         * Searchs for a given name
+         */
+        let searchKey = 'Fundação Renova';
+        let nameInput = await driver.findElement(By.name('nome-filtro'));
+        nameInput.sendKeys(searchKey);
+
+        let submitButton = await driver.findElement(By.className('btn-azul md-raised md-button uppercase md-full nomargin nopadding md-button md-ink-ripple'));
+        await submitButton.click();
+    }
+    catch (error) {
+        console.log('Failed at Asserting Search Fields');
+        console.log(error);
+        
+        failedTests += 1;
+    }
+    finally {
+        totalTests += 1;
+    }
+
+    /**
+     * Test search using the name / description filter and a location
+     * Itens should contain the search key in its name or sponsor's name regarding a partcular location
+     */
+    try {
+        await driver.get('https://prosas.com.br/editais');
+
+        /**
+         * Searchs for a given name and and location
+         */
+        let searchKey = 'Fundação Renova';
+        let nameInput = await driver.findElement(By.name('nome-filtro'));
+        nameInput.sendKeys(searchKey);
+        
+        
+        let locationKey = 'São Paulo';
+        let locationInput = driver.findElement(By.id('fl-input-10'));
+        locationInput.sendKeys(locationKey);
+        
+        let locationList = await driver.findElement(By.id('ul-10'));
+        let locations = await locationList.findElements(By.className('ng-scope'));
+
+        let locationExactKey = 'São Paulo SP';
+        for (let locationField of locations) {
+            let location = await locationField.getText();
+            
+            if (location === locationExactKey) {
+                await locationField.click();
+            }
+        }
+
+        let submitButton = await driver.findElement(By.className('btn-azul md-raised md-button uppercase md-full nomargin nopadding md-button md-ink-ripple'));
+        await submitButton.click();
+    }
+    catch (error) {
+        console.log('Failed at Asserting Search Fields');
+        console.log(error);
+        failedTests += 1;
+    }
+    finally {
+        totalTests += 1;
+    }
+
+    await driver.quit();
+
+    if (failedTests > 0) {
+        console.log(failedTests + " of " + totalTests + " Tests Failed");
+        process.exit(1);
+    }
+    else {
+        console.log("All Tests Successfully Executed!!");
         process.exit(0);
     }
 })();
